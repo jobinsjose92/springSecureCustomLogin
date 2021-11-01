5dfb7d9deadcde74695089d3aa57d0ac34c297f5
jobinsjose92 <50811203+jobinsjose92@users.noreply.github.com>
2021-10-31 19:00:22 -0500

    login commit

---
+++ src/test/java/com/example/jobins/customLogin/CustomLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobins.customLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
